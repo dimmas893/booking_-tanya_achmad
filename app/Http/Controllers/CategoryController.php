3a68c4caa4dd5a2b1acc2d622a59fd849a2de32b
Ananda Dimmas Budiarto <68166970+dimmas893@users.noreply.github.com>
2022-08-11 16:53:44 +0700

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $category = Category::all();
+        return view('category.index', compact(['category']));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        Category::create([
+            'name' => $request->name,
+        ]);
+        toast('Data Category berhasil ditambahkan')->autoClose(2000)->hideCloseButton();
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // public function edit($id)
+    // {
+    //     $categories = Category::FindOrFail($id);
+    //     return view('category.edit', compact(['categories']));
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:users,name,' . $request->id,
+        ]);
+        $user = Category::find($request->id);
+        $user->update([
+            'name' => $request->name,
+        ]);
+        toast('Data Category berhasil diubah')->autoClose(2000)->hideCloseButton();
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $user = Category::find($request->id);
+        $user->delete();
+
+        toast('Data Category berhasil dihapus')->autoClose(2000)->hideCloseButton();
+        return redirect()->back();
+    }
+}
